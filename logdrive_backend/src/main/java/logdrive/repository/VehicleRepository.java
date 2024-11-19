049392420da7ebb12ed5a85b2e388b4e329bd5dc
Gabriel <zgabrieloliveir4@gmail.com>
2024-11-19 11:19:28 -0300

    fix: assinatura @repository em vehiclerepository

--- logdrive_backend/src/main/java/logdrive/repository/VehicleRepository.java
+++ logdrive_backend/src/main/java/logdrive/repository/VehicleRepository.java
@@ -2,9 +2,11 @@ package logdrive.repository;
 
 import logdrive.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findAllByDriverId(Long driverId);
 }
